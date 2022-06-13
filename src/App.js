a94e0f217ac6c2a6a312be35e517d7f82f35f2ba
Cooper Gamble <gamble.cooper23@gmail.com>
2022-06-12 23:57:14 -0500

    configured welcome page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
+import React, { useEffect } from 'react';
 import './App.css';
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
   const handleClick = () => {
-  setCount(count+1);
+    
+
   };
-    return (
-      <body>
-        <div id = "main">
-          <div id = "btn-container">
-            <button id = "btn" onClick = {handleClick}>Run for Ukraine</button>
-          </div>
-          <div id = "description">
-            <i>click to get started</i>
-          </div>
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  });
+  return (
+    <body>
+      <div id = "welcome">
+        <div id = "btn-container">
+          <button id = "btn" onClick = {handleClick}>Run for Ukraine</button>
         </div>
-      </body>
-    );
+        <div id = "description">
+          <i id = "desc-text">click to get started</i>
+        </div>
+      </div>
+
+
+    </body>
+  );
 }
 
 export default App;
