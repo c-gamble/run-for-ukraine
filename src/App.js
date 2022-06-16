5729f6651606f62b5216df8fefb3530890cbe8d9
Cooper Gamble <gamble.cooper23@gmail.com>
2022-06-16 12:52:46 -0500

    fixed page display errors

--- src/App.js
+++ src/App.js
@@ -8,121 +8,180 @@ import { CgPaypal } from 'react-icons/cg';
 import { IoLogoVenmo } from 'react-icons/io5';
 import { SiCashapp } from 'react-icons/si';
 function App() {
-  const menu = document.getElementById("menu");
-  const home = document.getElementById("home");
-  const info = document.getElementById("info");
-  const donate = document.getElementById("donate");
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  });
   const handleClick = () => {
-      menu.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+    const menu = document.getElementById("menu");
+    menu.scrollIntoView({ block: 'end',  behavior: 'smooth' });
   };
   const handleHome = () => {
+    const home = document.getElementById("home");
+    const info = document.getElementById("info");
+    const donate = document.getElementById("donate");
+
+    home.style.display = "flex";
+
     info.style.display = "none";
     donate.style.display = "none";
-
-    home.style.height = "100vh";
-    home.style.width = "100vw";
-    home.style.display = "flex";
-    home.style.flexDirection = "column";
-    home.style.justifyContent = "center";
-    home.style.background = "rgb(4,124,196)";
-    home.style.background = "-moz-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    home.style.background = "-webkit-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    home.style.background = "linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    home.style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#047cc4',endColorstr='#fbf404',GradientType=1);"
 
     home.scrollIntoView({ block: 'end', behavior: 'smooth' })
   }
   const handleInfo = () => {
+    const home = document.getElementById("home");
+    const info = document.getElementById("info");
+    const donate = document.getElementById("donate");
+
+    info.style.display = "flex";
+    
     home.style.display = "none";
     donate.style.display = "none";
 
-    info.style.height = "100vh";
-    info.style.width = "100vw";
-    info.style.display = "flex";
-    info.style.flexDirection = "column";
-    info.style.justifyContent = "center";
-    info.style.background = "rgb(4,124,196)";
-    info.style.background = "-moz-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    info.style.background = "-webkit-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    info.style.background = "linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    info.style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#047cc4',endColorstr='#fbf404',GradientType=1);"
-    
     info.scrollIntoView({ block: 'end', behavior: 'smooth' })
   }
   const handleDonate = () => {
+    const home = document.getElementById("home");
+    const info = document.getElementById("info");
+    const donate = document.getElementById("donate");
+
+    donate.style.display = "flex";
+ 
+
     home.style.display = "none";
     info.style.display = "none";
-
-    donate.style.height = "100vh";
-    donate.style.width = "100vw";
-    donate.style.display = "flex";
-    donate.style.flexDirection = "column";
-    donate.style.justifyContent = "center";
-    donate.style.background = "rgb(4,124,196)";
-    donate.style.background = "-moz-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    donate.style.background = "-webkit-linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    donate.style.background = "linear-gradient(90deg, rgba(4,124,196,1) 0%, rgba(251,244,4,1) 100%)";
-    donate.style.filter = "progid:DXImageTransform.Microsoft.gradient(startColorstr='#047cc4',endColorstr='#fbf404',GradientType=1);"
 
     donate.scrollIntoView({ block: 'end', behavior: 'smooth' })
   }
   const handleRegister = () => {
     window.open("https://forms.gle/Ma9YZb6VctfaAC8SA")
   };
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-  });
+
+  const homeMenu = () => {
+    const menu = document.getElementById('menu');
+    menu.scrollIntoView({ block: 'end',  behavior: 'smooth'});
+  };
+  const infoMenu = () => {
+    const menu = document.getElementById('menu');
+    menu.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+  };
+  const donateMenu = () => {
+    const menu = document.getElementById('menu');
+    menu.scrollIntoView({ block: 'end',  behavior: 'smooth' });
+  };
+
   return (
     <body>
         <div id = "welcome">
-          <div id = "btn-container">
-            <button id = "btn" onClick = {handleClick}>Run for Ukraine</button>
+          <div id = "welcome-top">
+
           </div>
-          <div id = "description">
-            <i id = "desc-text">click to get started</i>
+
+          <div id = "welcome-mid">
+            <div id = "btn-container">
+              <button id = "btn" onClick = {handleClick}>Run for Ukraine</button>
+            </div>
+            <div id = "description">
+              <i id = "desc-text">click to get started</i>
+            </div>
+          </div>
+
+          <div id = "bottom">
+            <div class = "footer" id = "w-sponsor">
+              <a id = "sponsor" href = "https://www.rochinitiative.org" target = "_blank" rel = "noreferrer">Sponsored By <b>RCI</b></a>
+            </div>
           </div>
         </div>
 
         <div id = "menu">
-          <div id = "nav-container">
-            <dl id = "navbar">
-              <dt id = "Home"><button id = "btn" onClick = {handleHome}>Home</button></dt>
-              <dd id = "subicon"><BiHomeAlt size = {30}/></dd>
-              <dt id = "Info"><button id = "btn" onClick = {handleRegister}>Register</button></dt>
-              <dd id = "subicon"><MdAppRegistration size = {30}/></dd>
-              <dt id = "Home"><button id = "btn" onClick = {handleInfo}>Info</button></dt>
-              <dd id = "subicon"><AiOutlineInfoCircle size = {30}/></dd>
-              <dt id = "Info"><button id = "btn" onClick = {handleDonate}>Donate</button></dt>
-              <dd id = "subicon"><TbReportMoney size = {30}/></dd>
-            </dl>
+          <div id = "menu-top">
+            
           </div>
+
+          <div id = "menu-mid">
+            <div id = "nav-container">
+              <dl id = "navbar">
+                <dt id = "Home"><button id = "btn" onClick = {handleHome}>Home</button></dt>
+                <dd id = "subicon"><BiHomeAlt size = {30}/></dd>
+                <dt id = "Info"><button id = "btn" onClick = {handleRegister}>Register</button></dt>
+                <dd id = "subicon"><MdAppRegistration size = {30}/></dd>
+                <dt id = "Home"><button id = "btn" onClick = {handleInfo}>Info</button></dt>
+                <dd id = "subicon"><AiOutlineInfoCircle size = {30}/></dd>
+                <dt id = "Info"><button id = "btn" onClick = {handleDonate}>Donate</button></dt>
+                <dd id = "subicon"><TbReportMoney size = {30}/></dd>
+              </dl>
+            </div>
+          </div>
+
+          <div id = "bottom">
+            <div class = "footer" id = "m-sponsor">
+              <a id = "sponsor" href = "https://www.rochinitiative.org" target = "_blank" rel = "noreferrer">Sponsored By <b>RCI</b></a>
+            </div>
+          </div>
+
         </div>
 
         <div id = "home">
-          this is home
-          <button id = "return" onClick = {handleClick}>back to menu</button>
+          <div id = "home-top">
+
+          </div>
+
+          <div id = "home-mid">
+            this is home
+            <button id = "return" onClick = {homeMenu}>back to menu</button>
+          </div>
+
+          <div id = "home-bottom">
+            <div class = "footer" id = "h-sponsor">
+                <a id = "sponsor" href = "https://www.rochinitiative.org" target = "_blank" rel = "noreferrer">Sponsored By <b>RCI</b></a>
+            </div>
+          </div>
+
         </div>
 
         <div id = "info">
-          this is info
-          <button id = "return" onClick = {handleClick}>back to menu</button>
+          <div id = "info-top">
+            info
+          </div>
+
+          <div id = "info-mid">
+            <button id = "return" onClick = {infoMenu}>back to menu</button>
+          </div>
+
+          <div id = "info-bottom">
+            <div class = "footer" id = "i-sponsor">
+                <a id = "sponsor" href = "https://www.rochinitiative.org" target = "_blank" rel = "noreferrer">Sponsored By <b>RCI</b></a>
+            </div>
+          </div>
+
         </div>
 
         <div id = "donate">
-          <div id = "options-container">
-            <dl id = "options">
-                <dt id = "option"><button id = "btn" onClick = {handleHome}>Venmo</button></dt>
-                <dd id = "subicon"><IoLogoVenmo size = {30}/></dd>
-                <dt id = "option"><button id = "btn" onClick = {handleRegister}>PayPal</button></dt>
-                <dd id = "subicon"><CgPaypal size = {30}/></dd>
-                <dt id = "option"><button id = "btn" onClick = {handleInfo}>CashApp</button></dt>
-                <dd id = "subicon"><SiCashapp size = {30}/></dd>
-                <dt id = "option"><button id = "btn" onClick = {handleDonate}>GPay</button></dt>
-                <dd id = "subicon"><AiOutlineGoogle size = {30}/></dd>
-              </dl>
-            </div>
-          <button id = "return" onClick = {handleClick}>back to menu</button>
+          <div id = "donate-top">
+            donate
+            <button id = "return" onClick = {donateMenu}>back to menu</button>
+          </div>
 
+          <div id = "donate-mid">
+            <div id = "options-container">
+              <dl id = "options">
+                  <dt id = "option"><button id = "btn" onClick = {null}>Venmo</button></dt>
+                  <dd id = "subicon"><IoLogoVenmo size = {30}/></dd>
+                  <dt id = "option"><button id = "btn" onClick = {null}>PayPal</button></dt>
+                  <dd id = "subicon"><CgPaypal size = {30}/></dd>
+                  <dt id = "option"><button id = "btn" onClick = {null}>CashApp</button></dt>
+                  <dd id = "subicon"><SiCashapp size = {30}/></dd>
+                  <dt id = "option"><button id = "btn" onClick = {null}>GPay</button></dt>
+                  <dd id = "subicon"><AiOutlineGoogle size = {30}/></dd>
+                </dl>
+              </div>
+          </div>
+
+
+          <div id = "donate-bottom">
+            <div class = "footer" id = "d-sponsor">
+                <a id = "sponsor" href = "https://www.rochinitiative.org" target = "_blank" rel = "noreferrer">Sponsored By <b>RCI</b></a>
+            </div>
+          </div>
         </div>
     </body>
   );
